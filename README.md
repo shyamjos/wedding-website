ab6ccbfa77ef11fc93a19d02701d6e888b5698a5
Shyam Jos <shyamjosepp@gmail.com>
2023-09-30 20:26:41 +0530

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Source code for our wedding website (https://we.shyamjos.com/) , Based on HTML theme https://freehtml5.co/wedding-free-html5-bootstrap-template-for-wedding-websites
+
+To set the countdown date see below section in `js/clock.js`
+
+````
+  // Target future date/24 hour time/Timezone
+  let targetDate = moment.tz("2023-10-29 12:00", "Asia/Kolkata");
+````
---
+++ js/clock.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  let clock;
+
+  // Grab the current date
+  let currentDate = new Date();
+
+  // Target future date/24 hour time/Timezone
+  let targetDate = moment.tz("2023-10-29 12:00", "Asia/Kolkata");
+
+  // Calculate the difference in seconds between the future and current date
+  let diff = targetDate / 1000 - currentDate.getTime() / 1000;
+
+  if (diff <= 0) {
+    // If remaining countdown is 0
+    clock = $(".clock").FlipClock(0, {
+      clockFace: "DailyCounter",
+      countdown: true,
+      autostart: false
+    });
+    console.log("Date has already passed!")
+    
+  } else {
+    // Run countdown timer
+    clock = $(".clock").FlipClock(diff, {
+      clockFace: "DailyCounter",
+      countdown: true,
+      callbacks: {
+        stop: function() {
+          console.log("Timer has ended!")
+        }
+      }
+    });
+    
+    // Check when timer reaches 0, then stop at 0
+    setTimeout(function() {
+      checktime();
+    }, 1000);
+    
+    function checktime() {
+      t = clock.getTime();
+      if (t <= 0) {
+        clock.setTime(0);
+      }
+      setTimeout(function() {
+        checktime();
+      }, 1000);
+    }
+  }
+});
